402164e30b2f34bd1e1feb26aa29c3e49229a06e
Tomasz Koba <tomaszkoba@outlook.com>
2022-02-03 21:14:00 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Tomasz Koba on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
